c7c5468eb0b54b8284acd1e2b6d608d25a3028db
chavaraitzik <chavafelder@gmail.com>
2023-01-17 18:31:42 -0500

    Added the Spec

---
+++ Wig Stylist Spec.sql
@@ -0,0 +1,52 @@
+/*
+Hi. I am a wig stylist, and as my business expands, I am looking for an organized system to keep track of my customers' details, preferences and upcoming appointments. 
+
+I heard that you are a top company that will create a customized system according to my needs. I look forward to working with you.
+
+Please note that this table is only to keep track of my clients and not my profits.
+I would like to keep track of my clients' names, phone numbers, preferred contact method (either call, text, or whatsapp), and total number of appointments they've had to date.
+If a client has an appointment scheduled, I would like to see the appointment's date and time. I would like to see how they want their wig styled for their appointment as well. 
+I ask all new clients how they got to me so that I can keep track of my most effective marketing. Please include a marketing forum column in the table.
+For business purposes, I would also like to know the date they were first entered into the systems.
+
+Questions:
+Q. Do you have multiple clients with the same first and last name?
+A. No. If I already have a client with a particular name, I'll ask my new client to give me a nickname, middle initial, or something else to easily differentiate between them.
+Q. Do you have a policy of how far in advance a client can book an appointment?
+A. Yes, not more than 2 months in advance.
+Q. Do you ever double book appointments?
+A. No.
+Q. What is the maximum number of appointments each client can have on schedule?
+A. 3. I do not need to see past appointments. Please note that each appointment may need a different wig style. 
+Q. Are there specific style choices that you offer your clients, or they can ask for any styles?
+A. There are specific styles only. Either straight, wavy, curly, or crimpy.
+Q. Would you like to keep track of the time that your clients were entered into the system as well?
+A. No, the date is sufficient.
+
+Reports: 
+1) I would like to see the ways that people heard about me and how many people found out about me through each way.
+    Please provide a list of my marketing forums, ordered from most to least popular.
+2) I would like to be able to easily recognize my biggest clients. 
+    Please provide a list of my clients and each one's total number of appointments. Order from most to least appointments.
+3) I would like to easily see all my upcoming appointments and clients. 
+    Please provide a list of upcoming appointments with the client's name included. Order from closest to farthest away.
+4) I would like to be able to come into work each day, and see all my clients and their appointment times scheduled for that day. 
+    Please provide a way for me to view my appointments each day. Order from earliest to latest in the day.
+
+Sample Data:
+
+Clients that have appointments scheduled:
+Esti Cohen, 7324569876, WhatsApp, 12, 01/01/2023 at 1:20pm, curly, 01/09/2022 at 2:00pm, straight, 01/09/2022 at 2:20pm, wavy, Word of Mouth
+Chani Katzenstein, 7326490004, Call, 3, 01/05/2023 at 10:00am, crimpy, 01/05/2023 at 10:20am, curly, Magazine
+Yehudis Fisch, 7329087654, Text, 0, 01/05/2023 at 10:40am, straight, Word of Mouth
+Tami Felder, 7326578098, WhatsApp, 15, 01/02/2023 at 12:00pm, crimpy, 01/02/2023 at 12:20pm, wavy, WhatsApp Status
+Shani Schwartz, 7325076754, Text, 8, 01/01/2023 at 1:00pm, straight, 01/05/2023 at 2:00pm, curly, Business Card
+Esther Cohen, 7326789900, Call, 6, 01/05/2023 at 1:20pm, crimpy, 01/05/2023 at 1:40pm, curly Word of Mouth
+
+
+Clients that do not have any appointments scheduled:
+Tzipporah Simon, 7326098767, Call, 18, Business Card
+Shifra Hirsch, 7329086576, Text, 3, Word of Mouth
+Nechama Raitzik, 7320985690, WhatsApp, 7, Word of Mouth
+Penina Gellis, 7320678765, Call, 1, Magazine
+Breindy Stern, 7326577890, WhatsApp, 1, WhatsApp Status
